8635d1505b34b91dbeb85eea17b296da991d638c
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2016-06-28 17:12:47 +0100

    Instrument with New Relic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /*globals cloudantService:true */
 /*eslint-env node */
+require('newrelic');
 var express = require('express');
 var bodyParser = require('body-parser');
 var cfenv = require("cfenv");
